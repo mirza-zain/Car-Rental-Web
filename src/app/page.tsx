005f5e64a12548ca0d244d33adc0b76f645f3771
mirza-zain <mirzazain269@gmail.com>
2025-01-21 12:59:41 +0500

    Service Page Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ import Hero from "./Images/hero.png";
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen flex flex-col md:flex-row flex-1 justify-center items-center bg-black text-white p-2">
+      <div id="home" className="w-full h-screen flex flex-col md:flex-row flex-1 justify-center items-center bg-black text-white p-2">
           <div className="w-full md:w-1/2 flex justify-center items-center">
             <div className="mt-20 md:mt-0">
               <h2 className="text-4xl font-semibold">Plan Your Trip Now</h2>
-              <p className="text-lg font-medium">Get 20% OFF on your 1st Trip</p>
+              <p className="text-lg font-medium mt-1">Get 20% OFF on your 1st Trip</p>
+              <button className="px-4 py-2 border-4 border-[red] hover:bg-[red] mt-4" title="booknow">Book Now</button>
             </div>
           </div>
           <div className="w-full md:w-1/2 place-items-center">
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <div id='service' className='w-full h-screen flex flex-1 flex-col justify-center items-center p-5'>
+      <h2 className='text-4xl font-bold mt-5'>Customer Services</h2>
+      <div id='line' className='px-14 py-0.5 bg-[red]'></div>
+      <div id='cards' className='flex flex-col md:flex-row justify-center item-center text-black mt-12 flex-wrap'>
+
+        <div className='w-full md:w-[35%] h-1/2 flex-col p-8 me-4 mt-5 bg-[gray]'>
+          <div className='flex justify-center items-center'>
+            <i className="text-[red] text-2xl ri-user-heart-line"></i>
+            <h3 className="text-lg md:text-xl font-semibold">Special Rates on Car Booking</h3>
+          </div>
+          <div className='mx-20 px-1 py-0.5 bg-[red] opacity-50'></div>
+          <p className="text-sm font-medium text-justify p-2 mt-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed nonumy eirmod tempor invidunt ut labore et dolore magnaed aliquyam erat, sed diam voluptua. 
+            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+        </div>
+        <div className='w-full md:w-[35%] h-1/2 flex-col p-8 me-4 mt-5 bg-[gray]'>
+          <div className='flex justify-center items-center'>
+            <i className="text-[red] text-2xl ri-user-heart-line"></i>
+            <h3 className="text-lg md:text-xl font-semibold">Special Rates on Car Booking</h3>
+          </div>
+          <div className='mx-20 px-1 py-0.5 bg-[red] opacity-50'></div>
+          <p className="text-sm font-medium text-justify p-2 mt-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed nonumy eirmod tempor invidunt ut labore et dolore magnaed aliquyam erat, sed diam voluptua. 
+            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+        </div>
+        <div className='w-full md:w-[35%] h-1/2 flex-col p-8 me-4 mt-5 bg-[gray]'>
+          <div className='flex justify-center items-center'>
+            <i className="text-[red] text-2xl ri-user-heart-line"></i>
+            <h3 className="text-lg md:text-xl font-semibold">Special Rates on Car Booking</h3>
+          </div>
+          <div className='mx-20 px-1 py-0.5 bg-[red] opacity-50'></div>
+          <p className="text-sm font-medium text-justify p-2 mt-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed nonumy eirmod tempor invidunt ut labore et dolore magnaed aliquyam erat, sed diam voluptua. 
+            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+        </div>
+        <div className='w-full md:w-[35%] h-1/2 flex-col p-8 me-4 mt-5 bg-[gray]'>
+          <div className='flex justify-center items-center'>
+            <i className="text-[red] text-2xl ri-user-heart-line"></i>
+            <h3 className="text-lg md:text-xl font-semibold">Special Rates on Car Booking</h3>
+          </div>
+          <div className='mx-20 px-1 py-0.5 bg-[red] opacity-50'></div>
+          <p className="text-sm font-medium text-justify p-2 mt-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed nonumy eirmod tempor invidunt ut labore et dolore magnaed aliquyam erat, sed diam voluptua. 
+            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
